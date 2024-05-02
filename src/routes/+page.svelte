3bd5f0ebfa974dd11484e1f5d12dd8ac89a38464
Steven Jones <steven.jones@computerminds.co.uk>
2024-05-02 19:20:43 +0100

    Initial commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,113 @@
+<h1 class="text-center font-bold text-lg mb-1">EQUIPMENT LIST</h1>
+<p class="text-center mb-1">Available for use by all members free of charge on the Mount Nod Estate only</p>
+<p class="text-center mb-3">The number denotes the custodian for the item. These are listed on your membership card.</p>
+
+<div class="grid grid-cols-2 gap-4 px-4">
+<div>
+<table class="equipment"><caption>Ladders</caption>
+<tr><td>20’ Double Ladders	</td><td>12</td></tr>
+<tr><td>25’ Double Ladders</td><td>6, 10</td></tr>
+<tr><td>3-way Folding Ladder</td><td>5</td></tr>
+<tr><td>Ladder Stand-Off	</td><td>6</td></tr>
+<tr><td>Roof Ladder	</td><td>6</td></tr>
+</table>
+
+<table class="equipment">
+<caption>Tools</caption>
+<tr><td>Angle Grinder	</td><td>19</td></tr>
+<tr><td>Black and Decker Workmate	</td><td>4</td></tr>
+<tr><td>Drain Rods	</td><td>8, 19</td></tr>
+<tr><td>Drill Sharpener	</td><td>4</td></tr>
+<tr><td>Electric Tile Cutter	</td><td>13</td></tr>
+<tr><td>Heavy Duty Hammer Drill	</td><td>8</td></tr>
+<tr><td>Mitre Jigsaw	</td><td>19</td></tr>
+<tr><td>Pick Axe	</td><td>2</td></tr>
+<tr><td>Pop Riveter	</td><td>5, 13</td></tr>
+<tr><td>Power Sander	</td><td>27</td></tr>
+<tr><td>Sledge Hammer	</td><td>2</td></tr>
+<tr><td>Spirit Level (1 meter)</td><td>19</td></tr>
+<tr><td>Staple Gun	</td><td>5</td></tr>
+<tr><td>Stopcock Turnkey	</td><td>5, 19</td></tr>
+<tr><td>Tile Cutter	</td><td>5</td></tr>
+</table>
+
+<table class="equipment">
+<caption>Car maintenance</caption>
+<tr><td>Axle Stand	</td><td>7</td></tr>
+<tr><td>Trolley Jack	</td><td>7</td></tr>
+</table>
+</div>
+
+<div>
+
+<table class="equipment">
+<caption>Gardening Aids</caption>
+<tr><td>3’ Loppers	</td><td>24</td></tr>
+<tr><td>Cordless Hedge Trimmer	</td><td>19</td></tr>
+<tr><td>Electric Weed Burner	</td><td>19</td></tr>
+<tr><td>Extension Cable - 30m</td><td>19</td></tr>
+<tr><td>Fence Sprayer	</td><td>9</td></tr>
+<tr><td>Fertilizer Spreader	</td><td>6, 15</td></tr>
+<tr><td>Garden Rotary Cultivator	</td><td>3</td></tr>
+<tr><td>Garden Shredder	</td><td>5, 19</td></tr>
+<tr><td>Garden Spray	</td><td>5</td></tr>
+<tr><td>Garden Vac/Blower	</td><td>4</td></tr>
+<tr><td>Lawn Aerator	</td><td>6</td></tr>
+<tr><td>Lawn Scarifier	</td><td>1, 10</td></tr>
+<tr><td>Long Tree Pruners	</td><td>8</td></tr>
+<tr><td>Pole Hedge Trimmer	</td><td>19</td></tr>
+<tr><td>Pressure Washer	</td><td>19</td></tr>
+<tr><td>Sack Truck	</td><td>19</td></tr>
+<tr><td>Tree Saw	</td><td>8</td></tr>
+<tr><td>Wheelbarrow	</td><td>5, 13</td></tr>
+</table>
+
+<table class="equipment">
+<caption>Household Aids</caption>
+<tr><td>Carpet Cleaner	</td><td>19</td></tr>
+<tr><td>Carpet Stretcher	</td><td>12</td></tr>
+<tr><td>Pasting Tables	</td><td>5, 19</td></tr>
+<tr><td>Pipe and Cable Detector	</td><td>13</td></tr>
+<tr><td>Steam Cleaner	</td><td>8</td></tr>
+<tr><td>Wallpaper Stripper	</td><td>1</td></tr>
+</table>
+
+</div>
+</div>
+
+<p class="text-center mt-2 mb-1 font-bold">An up-to-date list is available online: https://mnra.uk/</p>
+
+<p class="text-center mt-2">We ask all who use the equipment to treat it as if it was their own and return it promptly in a clean condition ready for the next user.</p>
+
+
+<div class="page-break"></div>
+<div class="flyer">
+<h1 class="text-center font-bold text-2xl mb-4">You are invited to:</h1>
+<h2 class="text-center font-bold text-3xl mb-4">MOUNT NOD RESIDENTS’ ASSOCIATION</h2>
+<h2 class="text-center font-bold text-3xl mb-4"><span class="underline">ANNUAL GENERAL MEETING</span></h2>
+<p class="text-center font-bold text-3xl mb-4">Thursday 6th June 2024</p>
+<p class="text-center font-bold text-3xl mb-4">7:30pm</p>
+<p class="text-center font-bold text-3xl mb-4">Mount Nod Primary School </p>
+
+<div class="px-6">
+<div class="mb-4">
+
+<p>The committee will give a report of the last year, information about the defibrillator, and will discuss the future of the association.</p>
+<p class="font-bold">Also present will be our local councillors who will give a report and answer questions.</p>
+
+</div>
+
+
+
+<p>All members of the association have access to the equipment listed on the reverse of this notice.</p>
+<p class="font-bold">Membership to the association costs £2 a year. You can join and receive your membership card:</p>
+<ul class="list-outside ml-6 list-disc mb-4">
+<li>At the AGM</li>
+<li>At the Vale, Nod Rise, Sat 15th June 1:00-3:00pm</li>
+<li>At the Vale, Nod Rise, Mon 17th June 7:00-9:00pm</li>
+<li>When an association collector visits your house in the next few months</li>
+</ul>
+<p class="text-center font-bold">THE ASSOCIATION DESPERATELY NEEDS NEW COLLECTORS, CUSTODIANS AND COMMITTEE MEMBERS, PLEASE CONTACT US IF YOU ARE INTERESTED IN JOINING.</p>
+<p class="text-center font-bold uppercase">WITHOUT NEW VOLUNTEERS THE ASSOCIATION will be unable TO CONTINUE IN ITS CURRENT FORM.</p>
+</div>
+</div>
