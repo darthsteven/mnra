7bc370916c9ca39233065a021c83f16474dc9519
Steven Jones <steven.jones@computerminds.co.uk>
2025-03-21 21:45:17 +0000

    Try to prerender.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 <script>
 	import '../app.css';
+  export const prerender = true;
 </script>
 
 <slot />
