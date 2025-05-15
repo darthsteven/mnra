5e8235ef7f5342ae9ee6ebdad7e0678f533275e6
Steven Jones <steven.jones@computerminds.co.uk>
2025-05-15 07:08:15 +0100

    Add AGM stuff

---
+++ src/components/AGMAgenda.svelte
@@ -0,0 +1,16 @@
+<div class="max-w-[80%]">
+    <h1 class="text-3xl mb-4">MNRA AGM 15th May 2025: Agenda</h1>
+    <ol class="list-outside list-decimal ml-4">
+        <li>Welcome</li>
+        <li>Apologies</li>
+        <li>Minutes of the last AGM</li>
+        <li>Matters arising from last AGM</li>
+        <li>Treasurer’s Report</li>
+        <li>Secretary's Report</li>
+        <li>Report on the defibrillator</li>
+        <li>What we do – and could do</li>
+        <li>Election of officers</li>
+        <li>Local councillors report</li>
+        <li>Any other business</li>
+    </ol>
+</div>
--- src/routes/agenda/+page.svelte
+++ src/routes/agenda/+page.svelte
@@ -1,18 +1,86 @@
 <script>
     import Agenda from "../../components/Agenda.svelte";
+    import AGMAgenda from "../../components/AGMAgenda.svelte";
 </script>
 
+    <div class="grid grid-cols-1 w-full justify-items-center gap-10">
+    <AGMAgenda />
+    <h2>Officer roles</h2>
     <div class="grid grid-cols-2 w-full justify-items-center gap-10">
-    <Agenda />
-    <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
-        <Agenda />
+        <div>
+            <h3>Chair</h3>
+            <h4>Leads the residents association and oversees its activities.</h4>
+            <ul class="list-outside list-disc ml-4">
+                <li>Chairs meetings and ensures everyone has a chance to contribute.</li>
+                <li>Helps set priorities and guides the committee's efforts.</li>
+                <li>Acts as a spokesperson for the association when needed.</li>
+                <li>Works closely with other officers to keep things running smoothly.</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Vice Chair</h3>
+            <h4>Supports the Chair and steps in when needed.</h4>
+            <ul class="list-outside list-disc ml-4">
+                <li>Assists the Chair in leading the association’s efforts.</li>
+                <li>Stands in for the Chair when they are unavailable.</li>
+                <li>Helps coordinate meetings and activities.</li>
+                <li>Works with other officers to ensure continuity and support.</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Secretary</h3>
+            <h4>Keeps the association organised and informed.</h4>
+            <ul class="list-outside list-disc ml-4">
+                <li>Takes notes (minutes) at meetings and shares them afterwards.</li>
+                <li>Manages correspondence and official records.</li>
+                <li>Helps schedule meetings and sends out notices.</li>
+                <li>Coordinates printing of flyers and membership cards etc.</li>
+                <li>Arranges and coordinates collectors to collect membership fees.</li>
+                <li>Ensures that the defibrillator is checked regularly and is maintained properly.</li>
+                <li>Manages the email newsletter and website</li>
+                <li>Supports communication between members.</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Treasurer</h3>
+            <h4>Looks after the association’s money.</h4>
+            <ul class="list-outside list-disc ml-4">
+                <li>Keeps track of income and spending.</li>
+                <li>Prepares simple financial reports for meetings and the AGM.</li>
+                <li>Ensures any money is spent properly and transparently.</li>
+                <li>Receives funds from collectors to put into the association bank account.</li>
+                <li>Purchases new equipment as needed.</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Equipment Manager</h3>
+            <h4>Manages any shared equipment the association owns.</h4>
+            <ul class="list-outside list-disc ml-4">
+                <li>Keeps a list of items and where they are stored.</li>
+                <li>Checks items are maintained and in good condition.</li>
+                <li>Lets the committee know if anything needs replacing or repairing.</li>
+                <li>Coordinates with the secretary to update the website with details of the equipment.</li>
+            </ul>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
     </div>
